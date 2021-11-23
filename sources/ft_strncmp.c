efa87535a30f9cc3ffa7f50823be9865615e8554
Nikolai Schumacher <nikolai@Nikolais-MacBook-Pro.local>
2021-11-23 22:55:39 +0100

    fixed little mistake i made in strncmp

--- sources/ft_strncmp.c
+++ sources/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nikolai <nikolai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 13:17:17 by nschumac          #+#    #+#             */
-/*   Updated: 2021/11/22 22:09:50 by nikolai          ###   ########.fr       */
+/*   Updated: 2021/11/23 22:51:21 by nikolai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	size_t			count;
 
 	count = 0;
-	while (count + 1 < n && str1[count] != '\0' && str2[count] != '\0')
+	while (count + 1 < n && str1[count] != '\0' && str1[count] == str2[count])
 		count++;
 	return (str1[count] - str2[count]);
 }
