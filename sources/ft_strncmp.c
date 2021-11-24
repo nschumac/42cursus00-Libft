79c3157f84df574054e9b7f6a3a5a1be1568420d
Nikolai Schumacher <nikolai@Nikolais-MacBook-Pro.local>
2021-11-24 17:53:55 +0100

    fixed strncmp

--- sources/ft_strncmp.c
+++ sources/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nikolai <nikolai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 13:17:17 by nschumac          #+#    #+#             */
-/*   Updated: 2021/11/23 22:51:21 by nikolai          ###   ########.fr       */
+/*   Updated: 2021/11/24 17:53:15 by nikolai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	size_t			count;
 
 	count = 0;
+	if (n == 0)
+		return (0);
 	while (count + 1 < n && str1[count] != '\0' && str1[count] == str2[count])
 		count++;
 	return (str1[count] - str2[count]);
